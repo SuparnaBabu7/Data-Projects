e94a12a9c0d43b61a08168f264eaebee1427def7
SuparnaBabu7 <161495664+SuparnaBabu7@users.noreply.github.com>
2025-03-26 22:21:16 +0530

    Committing 2 items from workspace c4b18156-c82f-43aa-bc98-72a760581027

---
+++ Malli_WH.Warehouse/dbo/Tables/emp_details.sql
@@ -0,0 +1,13 @@
+CREATE TABLE [dbo].[emp_details] (
+
+	[emp_id] int NULL, 
+	[first_name] varchar(8000) NULL, 
+	[last_name] varchar(8000) NULL, 
+	[salary] float NULL, 
+	[joining_date] datetime2(6) NULL, 
+	[department] varchar(8000) NULL, 
+	[gender] varchar(8000) NULL, 
+	[job_title] varchar(8000) NULL, 
+	[full_name] varchar(255) NULL
+);
+
---
+++ Malli_WH.Warehouse/dbo/Tables/project_details.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[project_details] (
+
+	[project_id] int NULL, 
+	[emp_id_no] int NULL, 
+	[project_name] varchar(8000) NULL, 
+	[start_date] datetime2(6) NULL, 
+	[end_date] datetime2(6) NULL, 
+	[status] varchar(8000) NULL
+);
+
